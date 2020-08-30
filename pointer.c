7a825f4df48536c27dc98be0b55050f9f5e449b8
Priya Sinha <67830472+priya-sinha11@users.noreply.github.com>
2020-08-30 14:41:43 +0530

    Pointer

--- pointer.c
+++ pointer.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int var=20;
-    int *p;
-    p=&var;
-    printf("Address of var :%x\n",&var);
-    printf("Address of *p :%x\n",p);
-    printf("Value of *p :%d\n",*p);
+    int var = 20;
+    int *p = &var;
+    printf("Address of var : %p\n",&var);
+    printf("Address of variable: %p\n",p);
+    printf("Address of pointer : %p\n", &p);
+    printf("Value of a :%d\n",*p);
+    printf("Value of var : %d\n",a);
     return 0;
 }
